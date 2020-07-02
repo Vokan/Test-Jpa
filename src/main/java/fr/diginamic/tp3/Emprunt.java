c26079cf583ad82612a5aa116fdbb4b27770d762
Vokan Kocak <vokankocak@gmail.com>
2020-07-03 01:15:35 +0200

    TP3

--- src/main/java/fr/diginamic/tp3/Emprunt.java
+++ src/main/java/fr/diginamic/tp3/Emprunt.java
@@ -7,9 +7,11 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
 
 /**
  * @author vokankocak
@@ -28,6 +30,91 @@ public class Emprunt {
 	
 	@Column(name="DELAI")
 	private Integer delai;
+	
+	@ManyToOne
+	@JoinColumn(name= "ID_CLIENT")
+	private Integer idClient;
+	
+	@Column (name ="ID")
+	private Integer id;
+	
+	/**
+	 * Constructeur
+	 *
+	 */
+	public Emprunt() {
+		super();
+	}
+
+	/** Getter
+	 * @return the dateDebut
+	 */
+	public LocalDate getDateDebut() {
+		return dateDebut;
+	}
+
+	/** Setter
+	 * @param dateDebut the dateDebut to set
+	 */
+	public void setDateDebut(LocalDate dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	/** Getter
+	 * @return the dateFin
+	 */
+	public LocalDate getDateFin() {
+		return dateFin;
+	}
+
+	/** Setter
+	 * @param dateFin the dateFin to set
+	 */
+	public void setDateFin(LocalDate dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	/** Getter
+	 * @return the delai
+	 */
+	public Integer getDelai() {
+		return delai;
+	}
+
+	/** Setter
+	 * @param delai the delai to set
+	 */
+	public void setDelai(Integer delai) {
+		this.delai = delai;
+	}
+
+	/** Getter
+	 * @return the idClient
+	 */
+	public Integer getIdClient() {
+		return idClient;
+	}
+
+	/** Setter
+	 * @param idClient the idClient to set
+	 */
+	public void setIdClient(Integer idClient) {
+		this.idClient = idClient;
+	}
+
+	/** Getter
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/** Setter
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	
 	
 
---
+++ src/main/java/fr/diginamic/tp3/TestBibliotheque.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.diginamic.tp3;
+
+/**
+ * @author vokankocak
+ *
+ */
+public class TestBibliotheque {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
